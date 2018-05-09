d821ca6b7ca1239725d1d2a6bec55c34cd1153cd
Prakash Bhatta <pbhatt48@gmail.com>
2018-05-09 16:05:13 -0500

    updating htmls

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -106,3 +106,14 @@ STATIC_URL = '/static/'
 #Registration
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = "/store/"
+
+#Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "pbhatt48@gmail.com"
+EMAIL_HOST_PASSWORD = "******"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "books@mysterybooks.com"
+
