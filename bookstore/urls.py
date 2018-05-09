8b78de8910fb0fff3ad58b3f88face56522db415
Prakash Bhatta <pbhatt48@gmail.com>
2018-05-09 13:42:51 -0500

    updates to add register

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+#from django.core.urlresolvers import reverse
+
 #from MasterPythonClass.MVCBOOKSTORESITE.Development.bookstore import store
 
 urlpatterns = [
@@ -9,5 +11,6 @@ urlpatterns = [
     url(r'^store/', include('store.urls'), name='store'),
     #url(r'^admin/', include('admin.site.urls'), name='index'),
     #url(r'^store/', 'store.views.store', name='store'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
